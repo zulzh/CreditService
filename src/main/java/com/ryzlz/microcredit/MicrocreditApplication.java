b4f0f8f82710951b44b13f2e9c214338ed5d54ef
zulzh <452123084@qq.com>
2019-08-05 22:40:13 +0800

    init

---
+++ src/main/java/com/ryzlz/microcredit/MicrocreditApplication.java
@@ -0,0 +1,13 @@
+package com.ryzlz.microcredit;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MicrocreditApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MicrocreditApplication.class, args);
+    }
+
+}
